3a73e52d462f9e4d8beb9bbf369a984e9b4be2e0
Gallastegui Juan <gallastegui.maitane@uni.lan>
2021-09-29 12:36:08 +0200

    Medio hecho

--- NewTelApp/app/src/main/java/com/example/newtelapp/SecondFragment.java
+++ NewTelApp/app/src/main/java/com/example/newtelapp/SecondFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -16,19 +17,15 @@ import java.util.ArrayList;
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
-    private SecondFragment sf = new SecondFragment();
-    private  Datuak datuak = new Datuak(sf.getContext());
-    ArrayList<Produktua> produktuak;
+    //private SecondFragment sf;
+    private Datuak datuak;
+    private ArrayList<Produktua>produktuak;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-
-
-
-        produktuak =datuak.datuakItzuli();//datuak arraylistean
-
+            Bundle savedInstanceState
+    ) {
 
 
         binding = FragmentSecondBinding.inflate(inflater, container, false);
@@ -38,6 +35,11 @@ public class SecondFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        //sf=new SecondFragment();
+        datuak=new Datuak(view.getContext());
+        produktuak=datuak.datuakItzuli();
+        TextView tx= (TextView) getView().findViewById(R.id.textViewIzenaInfo);
+        tx.setText(produktuak.get(0).getIzena());
 
         /**binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override

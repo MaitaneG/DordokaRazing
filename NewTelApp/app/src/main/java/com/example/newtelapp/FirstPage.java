d89b7dc8130f3e75c5480545f9f382aaab827087
Gallastegui Juan <gallastegui.maitane@uni.lan>
2021-10-05 13:36:02 +0200

    Adios Fragments

---
+++ NewTelApp/app/src/main/java/com/example/newtelapp/FirstPage.java
@@ -0,0 +1,56 @@
+package com.example.newtelapp;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class FirstPage extends AppCompatActivity {
+
+    private ImageButton irten_botoia;
+    private ImageButton erakutsi_produktuak_botoia;
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+
+        erakutsi_produktuak_botoia = findViewById(R.id.button_erakutsi_produktuak);
+        erakutsi_produktuak_botoia.setOnClickListener(this::bestePantilaraJoan);
+        irten_botoia = findViewById(R.id.button_irten);
+        irten_botoia.setOnClickListener(this::itxi);
+    }
+
+    public void bestePantilaraJoan(View view){
+        Intent myIntent = new Intent(view.getContext(), SecondPage.class);
+        myIntent.putExtra("id", 5);
+        startActivity(myIntent);
+    }
+
+    public void itxi(View view){
+        new AlertDialog.Builder(this)
+                .setTitle("Aplikazioa ixten ")
+                .setMessage("Aplikazioa itxi nahi duzu?")
+
+                // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Continue with delete operation
+                        System.exit(0);
+                    }
+                })
+
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+
+
+    }
+}
--- NewTelApp/app/src/main/java/com/example/newtelapp/SecondPage.java
+++ NewTelApp/app/src/main/java/com/example/newtelapp/SecondPage.java
@@ -1,29 +1,21 @@
 package com.example.newtelapp;
 
-import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.TranslateAnimation;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.newtelapp.databinding.FragmentSecondBinding;
 
 import java.util.ArrayList;
 
-public class SecondFragment extends Fragment {
+public class SecondPage extends AppCompatActivity {
 
-    private FragmentSecondBinding binding;
-    //private SecondFragment sf;
     private Datuak datuak;
     private ArrayList<Produktua>produktuak;
     private TextView izena;
@@ -39,51 +31,31 @@ public class SecondFragment extends Fragment {
     public Integer[] nireIrudiak = {R.drawable.i1, R.drawable.i2, R.drawable.i3, R.drawable.i4};
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-
-
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+        hasieratu();
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        datuak=new Datuak(view.getContext());
+    private void hasieratu() {
+        datuak=new Datuak(this);
         produktuak=datuak.datuakItzuli();
 
-        izena= (TextView) getView().findViewById(R.id.textViewIzenaInfo);
-        kategoria=(TextView)getView().findViewById(R.id.textViewKategoriaInfo);
-        prezioa=(TextView) getView().findViewById(R.id.textViewPrezioaInfo);
-        kantitatea=(TextView)getView().findViewById(R.id.textViewKantitateaInfo);
-        irudia=view.findViewById(R.id.irudia);
+        izena=findViewById(R.id.textViewIzenaInfo);
+        kategoria=findViewById(R.id.textViewKategoriaInfo);
+        prezioa=findViewById(R.id.textViewPrezioaInfo);
+        kantitatea=findViewById(R.id.textViewKantitateaInfo);
+        irudia=findViewById(R.id.irudia);
 
-        botoiaAurrera=view.findViewById(R.id.button_produktuak_aurrera);
-        botoiaAtzera=view.findViewById(R.id.button_produktuak_atzera);
-        botoiaIrten=view.findViewById(R.id.button_irten);
+        botoiaAurrera=findViewById(R.id.button_produktuak_aurrera);
+        botoiaAtzera=findViewById(R.id.button_produktuak_atzera);
+        botoiaIrten=findViewById(R.id.button_irten);
 
         botoiaAurrera.setOnClickListener(this::produktuakAurrera);
         botoiaAtzera.setOnClickListener(this::produktuakAtzera);
         botoiaIrten.setOnClickListener(this::irten);
 
         datuakAldatu();
-
-        /**binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });**/
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
     }
 
     public void datuakAldatu(){
@@ -95,7 +67,7 @@ public class SecondFragment extends Fragment {
     }
 
     public void animation(){
-        Animation an = AnimationUtils.loadAnimation(getContext(),R.anim.to_right);
+        Animation an = AnimationUtils.loadAnimation(this,R.anim.to_right);
         izena.startAnimation(an);
         kategoria.startAnimation(an);
         prezioa.startAnimation(an);
@@ -124,8 +96,8 @@ public class SecondFragment extends Fragment {
     }
 
     public void irten(View view){
-        NavHostFragment.findNavController(SecondFragment.this)
-                .navigate(R.id.action_SecondFragment_to_FirstFragment);
+        Intent myIntent = new Intent(view.getContext(), FirstPage.class);
+        startActivity(myIntent);
     }
 
 }

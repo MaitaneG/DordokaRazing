ca1706a985ded4ef46de149795092bc6bc5899be
agerKalboetxeaga <75113781+agerKalboetxeaga@users.noreply.github.com>
2021-10-08 13:09:34 +0200

    Add files via upload

--- Lanzador_Oddo/src/lanzador/Lanzador.java
+++ Lanzador_Oddo/src/lanzador/Lanzador.java
@@ -2,40 +2,51 @@ package lanzador;
 
 import java.io.File;
 
-
-
 public class Lanzador {
 	/**
-	 * Metodo honek processBuilder objetuak gauzatzen ditu bidalitako bi parametroekin
+	 * Metodo honek processBuilder objetuak gauzatzen ditu bidalitako bi
+	 * parametroekin
+	 * 
 	 * @param url da nabigatzailea zabaltzerakoan redirekzionatzeko helbidea
-	 * @param c b (bai), e(ez) ea beste aplikazioa exekutatzea nahi duzun
+	 * @param c   b (bai), e(ez) ea beste aplikazioa exekutatzea nahi duzun
 	 */
-	public void lanzarOddo( String url, char c) {  
-		ProcessBuilder pb, pb2;												//Prozesuen deklarazioa
+	public void lanzarOddo(String url, char c) {
+		ProcessBuilder pb, pb2; // Prozesuen deklarazioa
 		try {
-			
-			String directory= System.getProperty("user.dir") + "\\bin\\";	//Proiektuaren oraingo ruta
-			
-			//pb2 = new ProcessBuilder("");
-			//pb2.start();
-	
-			pb = new ProcessBuilder("java.exe", "-cp", directory, "lanzador.Navegador", url, Character.toString(c));	//prozesu berri bat proiektuko beste klase bat exekutatzen duena, urla eta char-a bidaltzen dio
-			//pb.redirectOutput(new File(fichResultado));
-			pb.redirectError(new File("Error_log.txt"));					//Erroreak Error_log fitxategian idazten dira
-			pb.start();//proceso ha iniciado
-		
-			
+
+			String directory = System.getProperty("user.dir") + "\\bin\\"; // Proiektuaren oraingo ruta
+
+			// pb2 = new ProcessBuilder("");
+			// pb2.start();
+
+			pb = new ProcessBuilder("java.exe", "-cp", directory, "lanzador.Navegador", url, Character.toString(c)); // prozesu
+																														// berri
+																														// bat
+																														// proiektuko
+																														// beste
+																														// klase
+																														// bat
+																														// exekutatzen
+																														// duena,
+																														// urla
+																														// eta
+																														// char-a
+																														// bidaltzen
+																														// dio
+			// pb.redirectOutput(new File(fichResultado));
+			pb.redirectError(new File("Error_log.txt")); // Erroreak Error_log fitxategian idazten dira
+			pb.start();// proceso ha iniciado
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 	public static void main(String[] args) {
-		Lanzador l = new Lanzador();										//Klaseko objetu berri bat sortu
-		
-		l.lanzarOddo(" \"http://25.32.59.79:8069/web/login\" ", 'b' );		//Metodoari deitu
+		Lanzador l = new Lanzador(); // Klaseko objetu berri bat sortu
+
+		l.lanzarOddo(" \"http://25.32.59.79:8069/web/login\" ", 'b'); // Metodoari deitu
 		System.out.println("Ok");
 	}
-	
 
 }
--- Lanzador_Oddo/src/lanzador/Navegador.java
+++ Lanzador_Oddo/src/lanzador/Navegador.java
@@ -4,34 +4,66 @@ import java.io.File;
 
 public class Navegador {
 	public static void main(String[] args) {
-		String url = args[0];				
-		char backup = (args[1].toCharArray())[0];							//Aldagaien deklarazioa
+		String url = args[0];
+		char backup = (args[1].toCharArray())[0]; // Aldagaien deklarazioa
 		ProcessBuilder pb, pb2;
-		
 
-		//String directory = "C:\\Users\\kalboetxeaga.ager\\Documents\\aab\\newDordoka3\\DordokaRazing\\odoKonekxioa";
+		// String directory =
+		// "C:\\Users\\kalboetxeaga.ager\\Documents\\aab\\newDordoka3\\DordokaRazing\\odoKonekxioa";
 
 		try {
-			
-			pb = new ProcessBuilder("\"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\"" , url);		//Prozesu bat sortzen da ProcessBuilder erabiliz
-			//pb.redirectOutput(new File(fichResultado));															//Chrome aplikazioa exekutatzen du eta url a pasatzen dio parametro bezala horrela chrome horrialde horretara joango da
-			pb.redirectError(new File("error_logs\\Odoo_Error_log.txt"));											//Odook izan dituen erroreak fitxategi batera bidaltzen ditu
-			pb.start();	//proceso ha iniciado
+
+			pb = new ProcessBuilder("\"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\"", url); // Prozesu
+																											// bat
+																											// sortzen
+																											// da
+																											// ProcessBuilder
+																											// erabiliz
+			// pb.redirectOutput(new File(fichResultado)); //Chrome aplikazioa exekutatzen
+			// du eta url a pasatzen dio parametro bezala horrela chrome horrialde horretara
+			// joango da
+			pb.redirectError(new File("error_logs\\Odoo_Error_log.txt")); // Odook izan dituen erroreak fitxategi batera
+																			// bidaltzen ditu
+			pb.start(); // proceso ha iniciado
 			System.out.println("ok");
-			
-			
-			if(backup == 'b') {	
 
+			if (backup == 'b') {
 
-				String value = System.getenv("USERPROFILE");														//Ordenagailua erabiltzen hari den erabiltzailearen ruta
-				pb2 = new ProcessBuilder(value + "\\Documents\\DordokaRazing\\odoKonekxioa\\ejecutador.bat");		//Hemen batch fitxategi bat exekutatzeko prozesu bat habiarazten da. Batch honek odooKonexioa proiektuko karpetara joango da eta mvn comandu baten bidez spring boot aplikazioa exekutatzen du backgroundean
+				String value = System.getenv("USERPROFILE"); // Ordenagailua erabiltzen hari den erabiltzailearen ruta
+				pb2 = new ProcessBuilder(value + "\\Documents\\DordokaRazing\\odoKonekxioa\\ejecutador.bat"); // Hemen
+																												// batch
+																												// fitxategi
+																												// bat
+																												// exekutatzeko
+																												// prozesu
+																												// bat
+																												// habiarazten
+																												// da.
+																												// Batch
+																												// honek
+																												// odooKonexioa
+																												// proiektuko
+																												// karpetara
+																												// joango
+																												// da
+																												// eta
+																												// mvn
+																												// comandu
+																												// baten
+																												// bidez
+																												// spring
+																												// boot
+																												// aplikazioa
+																												// exekutatzen
+																												// du
+																												// backgroundean
 
-				
-				pb2.redirectError(new File("error_logs\\Java_Error_log.txt"));										//Erroreak Java error logera bidaltzen dira
+				pb2.redirectError(new File("error_logs\\Java_Error_log.txt")); // Erroreak Java error logera bidaltzen
+																				// dira
 				pb2.start();
-				System.out.println("Backup created" );
+				System.out.println("Backup created");
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}

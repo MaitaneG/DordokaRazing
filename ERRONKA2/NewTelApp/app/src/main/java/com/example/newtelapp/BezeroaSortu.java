d69fe20266b2799ef9332ef215d4dde756571722
MaitaneG <Maitane>
2021-10-22 19:15:04 +0200

    EditText with border radius

--- ERRONKA2/NewTelApp/app/src/main/java/com/example/newtelapp/BezeroaSortu.java
+++ ERRONKA2/NewTelApp/app/src/main/java/com/example/newtelapp/BezeroaSortu.java
@@ -53,7 +53,7 @@ public class BezeroaSortu extends AppCompatActivity {
         Intent myIntent = new Intent(view.getContext(), Aurrekontua.class);
         // Animazioak definitu
         ActivityOptions options=ActivityOptions.makeCustomAnimation(this,R.anim.from_right, R.anim.from_right);
-        
+
         // Activity hau itxi eta besteari abisatu
         myIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         myIntent.putExtra("EXIT", true);

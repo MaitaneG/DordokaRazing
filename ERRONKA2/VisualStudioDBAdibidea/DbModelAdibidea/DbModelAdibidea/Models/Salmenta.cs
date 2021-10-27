fe72eb14ea044d839f4be9930dfc26cf32267e30
Xuinaga <jon.sui@hotmail.com>
2021-10-27 12:14:43 +0200

    Visual Adibidea

---
+++ ERRONKA2/VisualStudioDBAdibidea/DbModelAdibidea/DbModelAdibidea/Models/Salmenta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace DbModelAdibidea.Models
+{
+    [Table("SALMENTA")]
+    public partial class Salmenta
+    {
+        public decimal Id { get; set; }
+        public DateTime? Data { get; set; }
+        public double? Zenbatekoa { get; set; }
+        public string Nanbezeroa { get; set; }
+
+        public virtual Bezeroa NanbezeroaNavigation { get; set; }
+    }
+}

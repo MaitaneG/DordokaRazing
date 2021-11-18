1000a74d9a92274f2b898fec23adf58a9da3568d
MaitaneG <Maitane>
2021-11-18 20:27:49 +0100

    Visual Studio probatzen

--- ERRONKA2/Visual Studio Newtel/NewTelDesktopApp/NewTelDesktopApp/HasieraPantailaForm.Designer.cs
+++ ERRONKA2/Visual Studio Newtel/NewTelDesktopApp/NewTelDesktopApp/HasieraPantailaForm.Designer.cs
@@ -42,18 +42,21 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(263, 120);
+            this.label1.Location = new System.Drawing.Point(351, 148);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(237, 38);
+            this.label1.Size = new System.Drawing.Size(295, 48);
             this.label1.TabIndex = 0;
             this.label1.Text = "HASI SAIOA";
             // 
             // textBoxPasahitza
             // 
             this.textBoxPasahitza.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxPasahitza.Location = new System.Drawing.Point(258, 234);
+            this.textBoxPasahitza.Location = new System.Drawing.Point(344, 288);
+            this.textBoxPasahitza.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxPasahitza.Name = "textBoxPasahitza";
-            this.textBoxPasahitza.Size = new System.Drawing.Size(253, 26);
+            this.textBoxPasahitza.PasswordChar = '*';
+            this.textBoxPasahitza.Size = new System.Drawing.Size(336, 30);
             this.textBoxPasahitza.TabIndex = 2;
             this.textBoxPasahitza.Text = "Pasahitza";
             this.textBoxPasahitza.Click += new System.EventHandler(this.textBoxPasahitza_Click);
@@ -66,9 +69,10 @@
             this.buttonSubmitLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonSubmitLogin.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonSubmitLogin.ForeColor = System.Drawing.Color.White;
-            this.buttonSubmitLogin.Location = new System.Drawing.Point(258, 318);
+            this.buttonSubmitLogin.Location = new System.Drawing.Point(344, 391);
+            this.buttonSubmitLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonSubmitLogin.Name = "buttonSubmitLogin";
-            this.buttonSubmitLogin.Size = new System.Drawing.Size(117, 39);
+            this.buttonSubmitLogin.Size = new System.Drawing.Size(156, 48);
             this.buttonSubmitLogin.TabIndex = 3;
             this.buttonSubmitLogin.Text = "SARTU";
             this.buttonSubmitLogin.UseVisualStyleBackColor = false;
@@ -77,9 +81,10 @@
             // textBoxKorreoa
             // 
             this.textBoxKorreoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxKorreoa.Location = new System.Drawing.Point(258, 188);
+            this.textBoxKorreoa.Location = new System.Drawing.Point(344, 231);
+            this.textBoxKorreoa.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxKorreoa.Name = "textBoxKorreoa";
-            this.textBoxKorreoa.Size = new System.Drawing.Size(253, 26);
+            this.textBoxKorreoa.Size = new System.Drawing.Size(336, 30);
             this.textBoxKorreoa.TabIndex = 4;
             this.textBoxKorreoa.Text = "Korreo elektronikoa";
             this.textBoxKorreoa.Click += new System.EventHandler(this.textBoxKorreoa_Click);
@@ -92,9 +97,10 @@
             this.buttonIrten.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonIrten.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonIrten.ForeColor = System.Drawing.Color.White;
-            this.buttonIrten.Location = new System.Drawing.Point(393, 318);
+            this.buttonIrten.Location = new System.Drawing.Point(524, 391);
+            this.buttonIrten.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonIrten.Name = "buttonIrten";
-            this.buttonIrten.Size = new System.Drawing.Size(117, 39);
+            this.buttonIrten.Size = new System.Drawing.Size(156, 48);
             this.buttonIrten.TabIndex = 5;
             this.buttonIrten.Text = "IRTEN";
             this.buttonIrten.UseVisualStyleBackColor = false;
@@ -104,18 +110,19 @@
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(316, 283);
+            this.label2.Location = new System.Drawing.Point(421, 348);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(137, 13);
+            this.label2.Size = new System.Drawing.Size(183, 17);
             this.label2.TabIndex = 6;
             this.label2.Text = "Email edo pasahitza okerra!";
             // 
             // HasieraPantailaForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InfoText;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.buttonIrten);
             this.Controls.Add(this.textBoxKorreoa);
@@ -124,6 +131,7 @@
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "HasieraPantailaForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LOGIN";
